1bb0066a7f5c2e34be72f91e315d7c9d2f2fb6a2
Joain <joainmonroy12@gmail.com>
2025-02-15 11:40:18 +0100

    Primer commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+      assetsInlineLimit: 0,
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name][extname]',
+        }
+      }
+    }
+  }
+});
